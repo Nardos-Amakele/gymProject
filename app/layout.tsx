0fd322c5107b63f7908aa378bfa51612b439a81d
Nardos Amakele <nardosamakele@gmail.com>
2024-09-08 20:12:09 +0300

    footer and about content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 // app/layout.tsx
+//import '@fortawesome/fontawesome-svg-core/styles.css';
+//import { config, Config } from '@fortawesome/fontawesome-svg-core';
+//config.autoAddCss =false;
 
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Header from './components/Header'; 
 import styles from './styles/ButtonStyles.module.css'; 
+import Footer from './components/Footer'
+
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -54,8 +59,46 @@ export default function RootLayout({
 </div>
         </section>
 
-     
+<section className="text-white mb-24 px-8">
+  <div className="container mx-auto flex flex-col lg:flex-row items-center">
+    <div className="lg:w-1/2 mb-10 ml-16">
+      <div className="rounded-xl overflow-hidden">
+        <img
+          src="/images/gym-interior.jpg"
+          alt="Gym Interior"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+    <div className="lg:w-1/2 lg:pl-12 mr-16 lg:text-start sm:text-center">
+      <h2 className="text-xl font-bold mb-6 text-customBlue">About Us</h2>
+      <p className="text-base mb-6 leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+      </p>
+      <div className="flex space-x-4">
+        <div className="w-1/2">
+          <img
+            src="/images/image-1.jpg"
+            alt="Gym activity 1"
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="w-1/2">
+          <img
+            src="/images/image-2.jpg"
+            alt="Gym activity 2"
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="mt-8 lg:px-10 mx-auto container">
+        <hr className="border-t-8 border-customBlue w-full" />
+      </div>
+</section>
 
+        <Footer/> 
         {children}
       </body>
     </html>
