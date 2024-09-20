267d92fe62f70ea98b4126c81b8631d5fed81890
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-09-20 16:39:07 +0300

    service page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ServiceCard from './ServicesCards';
 import servicesHero from '../../assets/images/services_hero.jpg';
 
 const page = () => {
@@ -17,108 +18,165 @@ const page = () => {
         </div>
       </div>
 
-     <section className='px-[9rem]  '>
-         {/* Body Building Section */}
-      <section className="py-12">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h2 className="text-3xl font-bold text-[#2596BE]">Body Building</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
-            {/* Pricing Card 1 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">Monthly</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">1,100 Birr</p>
-            </div>
-            {/* Pricing Card 2 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">Yearly</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">11,500 Birr</p>
-            </div>
-            {/* Pricing Card 3 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">6 Months</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">6,000 Birr</p>
-            </div>
-            {/* Pricing Card 4 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">3 Months</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">3,100 Birr</p>
+      <section className="px-[3rem]">
+        {/* Body Building Section */}
+        <section className="py-12">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+            <h2 className="text-3xl font-bold text-[#2596BE] py-10">Body Building</h2>
+            <div className="flex flex-wrap gap-6 justify-center">
+              <ServiceCard
+                title="Monthly"
+                price="1,100 Birr"
+                benefits={[
+                  'Unlimited gym access with personalized bodybuilding coaching.',
+                  'Includes two 1:1 coaching sessions per week.',
+                  'Free access to one special bodybuilding event each month.',
+                ]}
+              />
+           
+              <ServiceCard
+                title="6 Months"
+                price="6,000 Birr"
+                benefits={[
+                  'Half-year access to gym facilities, including all bodybuilding equipment.',
+                  'Free nutritional tracking and monthly assessments.',
+                  'Discount on fitness and wellness workshops.',
+                ]}
+              />
+              <ServiceCard
+                title="3 Months"
+                price="3,100 Birr"
+                benefits={[
+                  'Access for three months with regular progress check-ins and assessments.',
+                  'Includes access to nutritional workshops and meal plans.',
+                  'Monthly performance tracking and progress reports.',
+                ]}
+              />
+                 <ServiceCard
+                isPremium= {true}
+                title="Yearly"
+                price="11,500 Birr"
+                benefits={[
+                  'Full year of access to all bodybuilding equipment and classes.',
+                  'Priority booking for 1:1 coaching and diet consultations.',
+                  'Exclusive discounts on supplements and apparel.',
+                ]}
+              />
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      {/* Exercise Section */}
-      <section className="py-12">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h2 className="text-3xl font-bold text-[#2596BE]">Exercise</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
-            {/* Pricing Card 1 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">Yearly</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">14,000 Birr</p>
-            </div>
-            {/* Pricing Card 2 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">6 Months</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">7,200 Birr</p>
-            </div>
-            {/* Pricing Card 3 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">3 Months</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">3,600 Birr</p>
-            </div>
-            {/* Pricing Card 4 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">20 Days (With Coupon)</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">1,000 Birr</p>
-            </div>
-            {/* Pricing Card 5 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">Monthly</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">1,300 Birr</p>
-            </div>
-          </div>
-        </div>
-      </section>
+        {/* Exercise Section */}
+        <section className="py-12">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+            <h2 className="text-3xl font-bold text-[#2596BE] pb-10">Exercise</h2>
+            <div className="flex flex-wrap gap-6 justify-center">
+          
+       
+              <ServiceCard
+                title="20 Days (With Coupon)"
+                price="1,000 Birr"
+                benefits={[
+                  '20 days of gym and class access with optional fitness tracking.',
+                  'Eligible for a discount on future memberships.',
+                  'Access to one free group fitness class.',
+                ]}
+              />
+              <ServiceCard
+                title="Monthly"
+                price="1,300 Birr"
+                benefits={[
+                  'One-month access to gym and classes with weekly fitness assessments.',
+                  'Access to member-only fitness workshops and discounts on training packages.',
+                ]}
+              />
+                     <ServiceCard
+                title="3 Months"
+                price="3,600 Birr"
+                benefits={[
+                  'Three months of access to all exercise equipment and classes.',
+                  'Weekly check-ins with a fitness coach and personalized workout plans.',
+                  'Free entry to one nutrition seminar.',
+                ]}
+              />
 
-      {/* Group Fitness Section */}
-      <section className="py-12">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h2 className="text-3xl font-bold text-[#2596BE]">Group Fitness</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
-            {/* Pricing Card 1 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">Group Fitness Only</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">1,800 Birr</p>
-            </div>
-            {/* Pricing Card 2 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">Group Fitness + Exercise</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">1,200 Birr</p>
+<ServiceCard
+                title="6 Months"
+                price="7,200 Birr"
+                benefits={[
+                  'Unlimited gym access with exclusive discounts on personal training services.',
+                  'Monthly goal assessments and free entry to selected fitness workshops.',
+                  'Includes optional nutritional guidance.',
+                ]}
+              />
+                  <ServiceCard
+                isPremium={true}
+                title="Yearly"
+                price="14,000 Birr"
+                benefits={[
+                  '12-month access to all exercise classes, equipment, and fitness consultations.',
+                  'Custom fitness programs and bi-weekly goal assessments.',
+                  'Access to members-only events and special sessions.',
+                ]}
+              />
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      {/* Personal Training Section */}
-      <section className="py-12">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h2 className="text-3xl font-bold text-[#2596BE]">Personal (Includes Diet + 1L Water)</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
-            {/* Pricing Card 1 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">20 Days</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">30,600 Birr</p>
-            </div>
-            {/* Pricing Card 2 */}
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-bold text-white">Monthly</h3>
-              <p className="text-4xl font-bold text-[#2596BE]">39,000 Birr</p>
+        {/* Group Fitness Section */}
+        <section className="py-12">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+            <h2 className="text-3xl font-bold text-[#2596BE] pb-10">Group Fitness</h2>
+            <div className="flex flex-wrap gap-6 justify-center">
+              <ServiceCard
+                isPremium={true}
+                title="Group Fitness Only"
+                price="1,800 Birr"
+                benefits={[
+                  'Access to all group fitness classes, including yoga, Zumba, and pilates.',
+                  'Discounts on fitness gear and supplements.',
+                  'Weekly fitness tracking and optional dietary consultations.',
+                ]}
+              />
+              <ServiceCard
+                title="Group Fitness + Exercise"
+                price="1,200 Birr"
+                benefits={[
+                  'Full access to group fitness classes and exercise equipment.',
+                  'Weekly coaching and nutrition tracking.',
+                  'Includes two special events per month.',
+                ]}
+              />
             </div>
           </div>
-        </div>
+        </section>
+
+        {/* Personal Training Section */}
+        <section className="py-12">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+            <h2 className="text-3xl font-bold text-[#2596BE] pb-10">Personal Training</h2>
+            <div className="flex flex-wrap gap-6 justify-center">
+              <ServiceCard
+                title="1-on-1 Coaching"
+                price="2,000 Birr / session"
+                benefits={[
+                  'Includes post-session progress analysis and fitness tracking.',
+                  'Custom workout plans and nutrition advice.',
+                ]}
+              />
+              <ServiceCard
+                isPremium={true}
+                title="Monthly Personal Training Package"
+                price="7,500 Birr / month"
+                benefits={[
+                  'Customized workout and nutrition plan tailored to your goals.',
+                  'Access to all fitness classes and gym equipment.',
+                ]}
+              />
+            </div>
+          </div>
+        </section>
       </section>
-     </section>
     </div>
   );
 };
