c64aaa957916efd043cadc32b87a9d3f5f44870f
Nardos Amakele <nardosamakele@gmail.com>
2024-09-25 20:00:44 +0300

    service page responsivness

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -32,7 +32,6 @@ const Page = () => {
       case 'Body Building':
         return (
           <>
-          
             <ServiceCard
               title="Monthly"
               price="1,100 Birr"
@@ -175,58 +174,57 @@ const Page = () => {
 
   return (
     <>
-          <Header/>
+      <Header/>
 
-    <div className="bg-black text-white space-y-6">
-      {/* Hero Section */}
-      <div
-        className="relative w-full h-[100vh] bg-fixed bg-center bg-cover"
-        style={{
-          backgroundImage: `url(${servicesHero.src})`,
-          backgroundAttachment: 'fixed',
-        }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <h1 className="text-6xl font-bold text-white">Services</h1>
-        </div>
-        {/* Upward Arrow */}
+      <div className="bg-black text-white space-y-6">
+        {/* Hero Section */}
         <div
-          onClick={scrollToNextSection}
-          className="absolute bottom-4 left-1/2 transform -translate-x-1/2 border border-white rounded-full p-4 cursor-pointer text-white"
+          className="relative w-full h-[100vh] bg-fixed bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${servicesHero.src})`,
+            backgroundAttachment: 'fixed',
+          }}
         >
-          <FontAwesomeIcon icon={faArrowUp} className='text-2xl' />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+            <h1 className="text-6xl font-bold text-white">Services</h1>
+          </div>
+          {/* Upward Arrow */}
+          <div
+            onClick={scrollToNextSection}
+            className="absolute bottom-4 left-1/2 transform -translate-x-1/2 border border-white rounded-full p-4 cursor-pointer text-white"
+          >
+            <FontAwesomeIcon icon={faArrowUp} className='text-2xl' />
+          </div>
+        </div>
+
+        {/* Tab Section */}
+        <div ref={nextSectionRef} className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+          <div className="bg-gray-800 rounded-2xl w-fit flex flex-wrap justify-center p-2 mx-auto space-x-4 mb-16 border-solid border">
+            {tabs.map((tab) => (
+              <button
+                key={tab}
+                onClick={() => setActiveTab(tab)}
+                className={`px-4 py-2 font-bold ${
+                  activeTab === tab
+                    ? 'text-[#2596BE] bg-gray-700'
+                    : 'text-gray-400 bg-gray-800'
+                } hover:bg-gray-600 transition-colors duration-200`}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+
+          {/* Description */}
+          <p className="text-center text-gray-400 mt-4">{descriptions[activeTab]}</p>
+
+          {/* Cards Section */}
+          <div className="flex flex-wrap gap-6 justify-center mt-8">
+            {renderCards()}
+          </div>
         </div>
       </div>
-
-      {/* Tab Section */}
-      <div ref={nextSectionRef} className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="bg-gray-800 rounded-2xl w-fit flex justify-center p-2 mx-auto space-x-4 mb-16 border-solid border">
-          {tabs.map((tab) => (
-            <button
-              key={tab}
-              onClick={() => setActiveTab(tab)}
-              className={`px-4 py-2 font-bold ${
-                activeTab === tab
-                  ? 'text-[#2596BE] bg-gray-700 ' 
-                  : 'text-gray-400 bg-gray-800'
-              } hover:bg-gray-600`}
-            >
-              {tab}
-            </button>
-          ))}
-        </div>
-
-        {/* Description */}
-        <p className="text-center text-gray-400 mt-4">{descriptions[activeTab]}</p>
-
-        {/* Cards Section */}
-        <div className="flex flex-wrap gap-6 justify-center mt-8">
-          {renderCards()}
-        </div>
-      </div>
-    </div>
-    <Footer/> 
-
+      <Footer/> 
     </>
   );
 };
