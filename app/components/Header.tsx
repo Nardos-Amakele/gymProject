76914d2a6a1b09ab47ff2e0c371d0a436f3bb4df
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-09-28 16:54:59 +0300

    login btn

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -36,39 +36,46 @@ const Header: React.FC = () => {
         <nav className={`md:flex ${menuOpen ? 'block' : 'hidden'} absolute md:static bg-transparent w-full md:w-auto top-12 right-0 md:right-auto z-10`}>
           <ul className="flex flex-col items-center md:flex-row text-base space-y-4 md:space-y-0 md:space-x-10 lg:space-x-14 p-6 md:p-0 ml-auto md:ml-0">
             <li className="cursor-pointer relative group">
-            <Link href="/">Home</Link>
+              <Link href="/">Home</Link>
               <span className="absolute left-1/2 bottom-0 transform -translate-x-1/2 h-[2px] w-0 bg-[#2596BE] transition-all duration-300 group-hover:w-8"></span>
             </li>
             <li className="cursor-pointer relative group">
-            <Link href="/about">About Us</Link>
+              <Link href="/about">About Us</Link>
               <span className="absolute left-1/2 bottom-0 transform -translate-x-1/2 h-[2px] w-0 bg-[#2596BE] transition-all duration-300 group-hover:w-8"></span>
             </li>
             <li className="cursor-pointer relative group">
-            <Link href="/services">Service</Link>
+              <Link href="/services">Service</Link>
               <span className="absolute left-1/2 bottom-0 transform -translate-x-1/2 h-[2px] w-0 bg-[#2596BE] transition-all duration-300 group-hover:w-8"></span>
             </li>
             <li className="cursor-pointer relative group">
-            <Link href="/Shop">Shop</Link>
+              <Link href="/Shop">Shop</Link>
               <span className="absolute left-1/2 bottom-0 transform -translate-x-1/2 h-[2px] w-0 bg-[#2596BE] transition-all duration-300 group-hover:w-8"></span>
             </li>
             <li className="cursor-pointer relative group">
-            <Link href="/Contact">Contact</Link>
+              <Link href="/Contact">Contact</Link>
               <span className="absolute left-1/2 bottom-0 transform -translate-x-1/2 h-[2px] w-0 bg-[#2596BE] transition-all duration-300 group-hover:w-8"></span>
             </li>
             {/* Sign-up Button in Mobile Menu */}
             <li className="mt-4 md:hidden">
               <button className={`${styles.customButton} w-full`}>
-                <Link href="/Register">Sign up</Link>
+                <Link href="/Register">SignUp</Link>
+              </button>
+              <button className={`bg-black text-customBlue w-full py-2 rounded-br-[1rem] hover:translate-y-1`}>
+                <Link href="/Login">LogIn</Link>
               </button>
             </li>
           </ul>
         </nav>
 
-        {/* Sign-up Button for Desktop */}
-        
-        <button className={`${styles.customButton} hidden md:inline-block`}>
-          <Link href="/Register">Sign up</Link>
-        </button>
+        {/* Sign-up and Login Buttons for Desktop */}
+        <div className="hidden md:flex ">
+          <button className={`${styles.customButton}` }>
+            <Link href="/Register">SignUp</Link>
+          </button>
+          <button className="bg-black py-[0.38rem] px-[1.8rem] rounded-br-[1rem] font-bold text-customBlue hover:shadow-[rgba(0, 0, 0, .3) 2px 8px 8px -5px] hover:translate-y-1">
+            <Link href="/Login">LogIn</Link>
+          </button>
+        </div>
     </header>
   );
 };
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -72,7 +72,7 @@ const Hero = () => {
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ delay: 0.6, duration: 1, ease: 'easeOut' }}
-              whileHover={{ scale: 1.1 }}
+              whileHover={{ scale: 1 }}
               whileTap={{ scale: 0.9 }}
             >
               <Link href="/Login">Join Now</Link>
