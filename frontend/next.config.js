ffaf3aa65050f8190a2162937d66335eee3ea4ee
JoshuaTaye <tayejoshua4@gmail.com>
2024-12-02 00:11:22 +0300

    feat: started implementation of mealPlan and meal schema.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,8 @@ const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    optimizeFonts: false,
+};
 
 module.exports = withNextIntl(nextConfig);
